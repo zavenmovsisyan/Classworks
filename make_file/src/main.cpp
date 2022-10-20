5126779a6857697e49d819d72e300205ab7709e0
Zaven <zaven.movsisyan.ec@gmail.com>
2022-10-21 00:20:58 +0400

    Added make_file:)

---
+++ make_file/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b);
+int square(int a);
+int min(int a, int b);
+int main(){
+    cout << sum(5,6) << endl;
+    cout << square(6) << endl;
+    cout << min(1,2) << endl;
+    return 0;
+}
+
---
+++ make_file/src/min.cpp
@@ -0,0 +1,6 @@
+int min(int a, int b){
+    if(a > b){
+        return b;
+    }
+    return a; 
+}
